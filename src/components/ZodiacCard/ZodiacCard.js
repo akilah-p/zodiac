6f989249cc694fa935a0c6773ac7f01bff227911
Akilah P <atpjun@gmail.com>
2023-01-11 07:48:31 -0500

    trying to fix prettier error for cs pass

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,4 +1,5 @@
 import './ZodiacCard.css';
+import zodiac from '../../data.js';
 export default function ZodiacCard({ name, dates, id, symbol }) {
     return (
         <div className="zodiac-card">
