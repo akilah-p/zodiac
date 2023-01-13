588337480d259627920d15e69d278bf02e1be337
Akilah <atpjun@gmail.com>
2023-01-13 07:45:23 -0500

    please pass ci

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -1,17 +1,20 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
+// eslint-disable-next-line prettier/prettier
 import zodiac from '../../data.js';
 import ZodiacCard from '../ZodiacCard/ZodiacCard.js';
 import backgroundImg from '../../background.png';
-import './Main.css';
+import './main.css';
 
 export default function Main() {
-    return (
-        <main style={{
-            backgroundImage: `url(${backgroundImg})`
-        }}>
-            {zodiac.map((item) => (
-                <ZodiacCard key={item.name} {...item} />
-            ))}
-        </main>
-    );
+  return (
+    <main
+      style={{
+        backgroundImage: `url(${backgroundImg})`
+      }}>
+      {zodiac.map((item) => (
+        <ZodiacCard key={item.name} {...item} />
+      ))}
+    </main>
+  );
 }
--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,12 +1,14 @@
-import './ZodiacCard.css';
-import zodiac from '../../data.js';
-export default function ZodiacCard({ name, dates, id, symbol }) {
-    return (
-        <div className="zodiac-card">
-            <img alt={name} src={`./signs/${id}.png`} />
-            <span className="name">{name}</span>
-            <span>{dates}</span>
-            <span>{symbol}</span>
-        </div>
-    );
+/* eslint-disable prettier/prettier */
+import './zodiacCard.css';
+import '../../data.js';
+
+export default function ZodiacCard({ name, dates, symbol }) {
+  return (
+    <div className="zodiac-card">
+      <img alt={name} src={`./signs/${name}.png`} />
+      <span className="name">{name}</span>
+      <span>{dates}</span>
+      <span>{symbol}</span>
+    </div>
+  );
 }
